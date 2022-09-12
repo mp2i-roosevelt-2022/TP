0fd5f910d8ef5be17662627cda8ef4242ab58733
Oudoire Gatien <Gatien-Oudoire>
2022-09-12 19:17:49 +0200

    Reecrit avec du code vu en cours

--- C-1/max.c
+++ C-1/max.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(void) {
     int nb1 = 0, nb2 = 0, max = 0;
+
     printf("Entrez les deux nombres séparés par un espace");
+
     scanf("%d %d", &nb1, &nb2);
-    max =  (nb1 > nb2) ? nb1 : nb2;
+
+    // On peut aussi utiliser l'operateur ternaire (pas au programme)
+    if (nb1 > nb2)
+        max = nb1;
+    else
+        max = nb2;
+
     printf("Le plus grand est %d", max);
+
     return 0;
 }
